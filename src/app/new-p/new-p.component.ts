03f6c7f18b6a1d4c23bd93daa48e5f08761ace92
Clife-Developer <clife.united@gmail.com>
2020-12-13 06:35:56 +0200

    login Form in Russian

--- src/app/new-p/new-p.component.ts
+++ src/app/new-p/new-p.component.ts
@@ -8,68 +8,15 @@ import { NgForm } from '@angular/forms';
 })
 export class NewPComponent implements OnInit {
 
-  obj:any;
-  constructor( private contactsService:ContactsService) {
+  constructor(){}
 
+  ngOnInit():void{
 
-   }
-  
-   
-  // passed=false;
-   addNewContact(form)
-   {
-     
-      console.log(form.value);
-      /*
-      console.log(form.value.title);
-      console.log(form.value.author);
-
-      const cont={
-                   id:form.value.id,
-                   title:form.value.title,
-                   author:form.value.author
-                 }
-  
-      this.contactsService.createContacts(cont).subscribe(data=>
-      {});
-
-*/
-
-   }
-
-   returnPar()
-   {
-     this.contactsService.getParams().subscribe(data=>{
-       console.log(data);
-     });
-   }
-
-   updateData(contactId)
-   {
-    const cont={
-      id:contactId,
-      title:"clife",
-      author:"Biko"
-    }
-
-      this.contactsService.updatedContacts(contactId,cont).subscribe(data=>
-      {  
-      });
-   }
-  
-   deleId(id)
-   {
-     this.contactsService.deleteData(id).subscribe((data)=>
-     {
-     });
-   }
-  ngOnInit(): void {
-  // this.obj=this.contactsService.getContacts();
-
-  this.contactsService.getContacts().subscribe(data=>
-    {
-      this.obj=data;
-    }); 
   }
 
+  addNewContact(formName)
+  {
+    
+    formName.reset();
+  }
 }
